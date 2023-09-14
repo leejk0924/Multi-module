5af6541ae405a1c449214cffe3e8502621066436
jk <akdirdl@gmail.com>
2023-09-15 00:44:12 +0900

    set Multi-module

---
+++ sp/files/src/main/java/com/example/project/Person.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Person {
+    private String name;
+
+}
---
+++ sp/files/src/test/java/com/example/project/PersonTest.java
@@ -0,0 +1,10 @@
+package com.example.project;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+    @Test
+    void getName() {
+
+    }
+}
